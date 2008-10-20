d29541151e5e8d268ea9dfc0a3ba72b6ab9c2670
Rowland Mosbergen <rowland.mosbergen@celentia.com>
2008-10-20 23:20:22 +0000

    Ability to delete with calendar using jquery

--- draggableCal/li.php
+++ draggableCal/li.php
@@ -2,7 +2,7 @@
 require_once('time.lib.php');
 
 //temp, 2b filled from db
-$blockedArray = array("10:30","13:00")
+//$blockedArray = array("10:30","13:00")
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" 
                     "http://www.w3.org/TR/html4/loose.dtd">
@@ -11,15 +11,72 @@ $blockedArray = array("10:30","13:00")
   	<script src="jquery-1.2.6.js"></script>
 	<script src="jquery-ui-personalized-1.6rc2.min.js"></script>
   	<script>
+  	
+  	function doubleClickSaved (name,count,start,secondTime,end,parent){
+  	
+  		var delSaved = confirm("Do you wish to delete?");
+  		
+  		if (delSaved){
+  			// convert each li back to normal.
+  			// each section has as a class the start date
+  			// for any child of the parent
+  			// eg. #timeslotsTuesday > li.07:15
+  			
+  			var txtStart = start.replace(/:/,"-");
+  			
+  			var query = "#" + parent + " > li."+txtStart;
+  			 
+  			
+  			var deleteCollection = jQuery(query);
+  			
+  			
+  			
+  			if(deleteCollection) {
+            	deleteCollection.each(function() {
+                	
+	                // now reset this back to what it used to be
+    	            
+        	        $(this).removeClass('saved').removeClass(txtStart);
+        	        
+           			
+                });		
+                  				
+                if (count > 2){
+                    deleteCollection.eq(0).text(start).removeClass('firstSaved');	
+                    deleteCollection.eq(1).text(secondTime).removeClass('saved').removeClass('firstSaved');
+                    deleteCollection.eq(count - 1).text(end).removeClass('lastSaved');
+                }
+		                    	
+                if (count == 1) {	
+                
+                 	deleteCollection.eq(0).text(start).removeClass('onlySaved');	
+                }
+                
+				if (count == 2) {	
+                   	deleteCollection.eq(0).text(start).removeClass('saved').removeClass('firstSaved');
+                   	deleteCollection.eq(1).text(end).removeClass('saved').removeClass('lastSaved');
+                    		
+                }	
+                
+                
+                
+  			}
+  			
+			  			
+  			
+  			
+  		} //if delsaved = y
+  	}
+  	
+  	
+  	
   	$(document).ready(function(){
 
 
         var collection;
           
-       	$('#timeslots').selectable({
+       	$('ul').selectable({
        	
-       		// filter: ".ui-selectable",
-       		
        		selecting: function(ev, ui) {
 
                if ($(ui.selecting).hasClass("saved")){
@@ -47,6 +104,11 @@ $blockedArray = array("10:30","13:00")
                 	var start = collection.eq(0).text();
                 	var end = collection.eq(count - 1).text();
                     
+                    
+                    // this is to keep the text of the second selected
+                    // li as it is used to display the date range
+                    var secondTime = collection.eq(1).text();
+                    
                     var startDate = new Date();
                     var endDate = new Date();
                     
@@ -68,49 +130,76 @@ $blockedArray = array("10:30","13:00")
                     
                     expectantCount = diffhours / 0.25; 
                     
+                    
+                    
+                    // check that there the difference between the start and end times match up to the number of counts we expect
                     if (expectantCount != count){
 						alert ("invalid selection");
 						// reset all the selections
+    	                collection.each(function() {
+        	            	//set the new height based on the height of the li height:20px  and bottom of 2px
+            	        	$(this).removeClass('ui-selected');
+           				
+                	    });						
 						
 					}
 					else {
 					
 						if (start != ""){
 						
-							// check that there the difference between the start and end times match up to the number of counts we expect
-							// alert (start + " :: " + end + " :: " + count);
 							
 							
-						
+							
 							var name = prompt(" please enter in the name of the appointment from " + start + " to " + end, "");
 								
+							var txtStart = start.replace(/:/,"-");
+														
+							if (name != null && name != ""){  							
+								// now clear out the ui-selected class from the selected panels
+								// and then set it to the saved class to block it out
+		    	                collection.each(function() {
+		                    	
+		                    		
+		                    	
+									//set the new height based on the height of the li height:20px  and bottom of 2px
+		            	        	$(this).removeClass('ui-selected').addClass(txtStart).addClass("saved");
+		           					$(this).unbind("click").click(function () { 
+	      	 							doubleClickSaved(name,count,start,secondTime,end,$(this).parent().attr('id'));
+			    					});    		
+		                	    });
+		                    
+		                    	// set the name of the person at the top
+		                    
+		                    	if (count > 2){
+		                    		collection.eq(0).text(name).removeClass('saved').addClass('firstSaved');	
+		                    		collection.eq(1).text(start + " to " + end).removeClass('saved').addClass('firstSaved');
+		                    		collection.eq(count - 1).addClass('lastSaved').removeClass('saved');		                    			
+		                    	}
+		                    	
+		                    	
+		                    	if (count == 1) {	
+		                    	
+		                    		collection.eq(0).text(name + " " + start).removeClass('saved').addClass('onlySaved');	
+		                    	}
+								if (count == 2) {	
+		                    		collection.eq(0).text(name + " " + start).removeClass('saved').addClass('firstSaved');
+		                    		collection.eq(1).removeClass('saved').addClass('lastSaved');
+		                    			
+		                    	}		                    	
+							                	
+							} // end of if name != ""	
+							else {
+								// reset all the selections
+    			                collection.each(function() {
+        	    	    	    	//set the new height based on the height of the li height:20px  and bottom of 2px
+            	    	    		$(this).removeClass('ui-selected');
+           					
+                	    		});						
 								
-							
-							// now clear out the ui-selected class from the selected panels
-							// and then set it to the saved class to block it out
-	    	                collection.each(function() {
-	                    	
+							}
+														
 								
-	                    	
-	        	            	//set the new height based on the height of the li height:20px  and bottom of 2px
-	            	        	$(this).removeClass('ui-selected').addClass("saved").selectable("disable").css("border-bottom","0px").text("").css("height","22px");
-	           				
-	                	    });
-	                    
-	                    	// set the name of the person at the top
-	                    
-	                    	if (count > 1){
-	                    		collection.eq(1).text(start + " to " + end);
-	                    		collection.eq(0).text(name);	
-	                    	}
-	                    	else {	
-	                    		collection.eq(0).text(name + " " + start);	
-	                    	}
-	                    	collection.eq(count - 1).css("height","20px").css("border-bottom","2px solid black");
-						                	
-							
-							
-						}
+						} // end of start !+ "" 
 					
 					}
 		
@@ -129,9 +218,12 @@ ul { list-style: none; margin:0px; padding:0px;}
 .ui-selecting { background: #CFD499; } 
 
 
-.saved {background: green }
+.firstSaved {background: green ; color: black; border-bottom: 0px; height: 12px;}
+.saved {background: green ; color: green; border-bottom: 0px; height: 12px;}
+.lastSaved {color: green; background: green ;border-bottom: 2px solid black; height: 10px;}
+.onlySaved {background: green ; color: black; border-bottom: 2px solid black; height: 10px;}
 
-li {border-bottom: 2px solid black;background: #CFD4E6; height:20px; width: 100px; margin-top:0px; font-size: 10px; font-family: Arial; padding-top: 3px; }
+li {border-bottom: 2px solid black;background: #CFD4E6; height:10px; width: 100px; margin-top:0px; font-size: 9px; font-family: Arial; padding-top: 3px; }
 <?php
 //Detect if user's browser is IE, if so, include IE hack to improve CSS Standards compliency
 $user_agent = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : '';
@@ -143,47 +235,27 @@ if(strpos($user_agent, 'MSIE') !== false)
 </style>
 
 
-
-
-
-
-
-
-<!--
-<style>
-ul { list-style: none; margin:0px; margin-left:-30px;}
-.ui-selected { background: #727EA3; color: #FFF; }
-.ui-selecting { background: #CFD499; }
-li { background: #CFD4E6; width: 100px; margin-top: 5px; font-size: 10px; font-family: Arial; padding-top: 3px; }
-.fakedLi { background: #CFD4E6; width: 100px; margin-top: 5px; margin-left:10px; font-size: 10px; font-family: Arial; padding-top: 3px; }
-</style>
--->
-
 </head>
 <body>
 
 
 <table CELLSPACING=0>
 	<tr><td>
-	<ul id="timeslotsLbl">
-	<?php
-	$timeslotArray = timeslotArray("08:00","13:30","15");
-	foreach($timeslotArray as $value){
-	?>
-		 <li style='background:#CCCCCC;color:black;'><?php echo $value;?></li>
 
-	<?php 
-	}
-	?>
-	</ul>
 
 	</td>
 	<td>
-
-	<ul id="timeslots">
+	Monday
+	<ul id="timeslotsMonday">
 	<?php
+	
+	$start = "07:00";
+	$end = "17:30";
+	$slots = "15";
+	$format = "24";
+	
 	$nBlocked=0;
-	$timeslotArray = timeslotArray("08:00","13:30","15","24");
+	$timeslotArray = timeslotArray($start,$end,$slots,$format);
 	foreach($timeslotArray as $value){
 		if(in_array($value, $blockedArray)){
 		$nBlocked++;
@@ -200,7 +272,123 @@ li { background: #CFD4E6; width: 100px; margin-top: 5px; font-size: 10px; font-f
 	}
 	?>
 	</ul>
-	</td></tr>
+	</td>
+	<td>
+	Tuesday
+	<ul id="timeslotsTuesday">
+	<?php
+	$nBlocked=0;
+	$timeslotArray = timeslotArray($start,$end,$slots,$format);
+	foreach($timeslotArray as $value){
+		if(in_array($value, $blockedArray)){
+		$nBlocked++;
+	?>
+		
+		<li class=saved style='background:black;color:white;'><?php echo $value;?></li>
+		
+	<?php
+		}else{
+	?>
+			<li><?php echo $value;?></li>
+	<?php
+		}
+	}
+	?>
+	</ul>	
+	</td>
+	<td>
+	Wednesday
+	<ul id="timeslotsWednesday">
+	<?php
+	$nBlocked=0;
+	$timeslotArray = timeslotArray($start,$end,$slots,$format);
+	foreach($timeslotArray as $value){
+		if(in_array($value, $blockedArray)){
+		$nBlocked++;
+	?>
+		
+		<li class=saved style='background:black;color:white;'><?php echo $value;?></li>
+		
+	<?php
+		}else{
+	?>
+			<li><?php echo $value;?></li>
+	<?php
+		}
+	}
+	?>
+	</ul>	
+	</td>
+	<td>
+	Thursday
+	<ul id="timeslotsThursday">
+	<?php
+	$nBlocked=0;
+	$timeslotArray = timeslotArray($start,$end,$slots,$format);
+	foreach($timeslotArray as $value){
+		if(in_array($value, $blockedArray)){
+		$nBlocked++;
+	?>
+		
+		<li class=saved style='background:black;color:white;'><?php echo $value;?></li>
+		
+	<?php
+		}else{
+	?>
+			<li><?php echo $value;?></li>
+	<?php
+		}
+	}
+	?>
+	</ul>	
+	</td>
+	<td>
+	Friday
+	<ul id="timeslotsFriday">
+	<?php
+	$nBlocked=0;
+	$timeslotArray = timeslotArray($start,$end,$slots,$format);
+	foreach($timeslotArray as $value){
+		if(in_array($value, $blockedArray)){
+		$nBlocked++;
+	?>
+		
+		<li class=saved style='background:black;color:white;'><?php echo $value;?></li>
+		
+	<?php
+		}else{
+	?>
+			<li><?php echo $value;?></li>
+	<?php
+		}
+	}
+	?>
+	</ul>	
+	</td>
+	<td>
+	Saturday
+	<ul id="timeslotsSaturday">
+	<?php
+	$nBlocked=0;
+	$timeslotArray = timeslotArray($start,$end,$slots,$format);
+	foreach($timeslotArray as $value){
+		if(in_array($value, $blockedArray)){
+		$nBlocked++;
+	?>
+		
+		<li class=saved style='background:black;color:white;'><?php echo $value;?></li>
+		
+	<?php
+		}else{
+	?>
+			<li><?php echo $value;?></li>
+	<?php
+		}
+	}
+	?>
+	</ul>	
+	</td>				
+	</tr>
 </table>
 </body>
 </html>
